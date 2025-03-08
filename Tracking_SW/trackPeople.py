1efe94b4ff6dc113a0cc2f73a479840145a47659
bcrudele <b.crudele627@gmail.com>
2025-03-08 16:09:22 -0500

    Combine process_camera() into process_video()

--- Tracking_SW/trackPeople.py
+++ Tracking_SW/trackPeople.py
@@ -2,67 +2,107 @@ import cv2
 from ultralytics import YOLO  # pip install ultralytics
 
 def print_info(x_avg_list, width_lower_bound, width_upper_bound):
-        if len(x_avg_list) != 0:
-            average_x = round(sum(x_avg_list) / len(x_avg_list),2)
-            people_in_frame = len(x_avg_list)
-            print(f"People in Frame: {people_in_frame} | x_avg -> {average_x}")
-            if (average_x > width_lower_bound):
-                print("move right")
-            elif (average_x < width_upper_bound):
-                print("move left")
+    """"
+    Prints:
+    - if camera should move left/right
+    - people in frame
+    - the average x coordinate of the people
+    """
+    if len(x_avg_list) != 0:
+        average_x = round(sum(x_avg_list) / len(x_avg_list),2)
+        people_in_frame = len(x_avg_list)
+        print(f"People in Frame: {people_in_frame} | x_avg -> {average_x}")
+        if (average_x > width_lower_bound):
+            print("move right")
+        elif (average_x < width_upper_bound):
+            print("move left")
 
-def process_video(video_path, model="yolov5s.pt", debuff_en=False, debuff_range=1, gui=True, debug=False, bound=0.1):
-    
-    # load YOLO
-    model = YOLO(model)
-
-    # start video capture
+def get_video_dims(video_path):
+    """
+    returns the source object, width, and height of the video
+    """
     cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        print("Error: Couldn't open video file.")
-        return
-
-    # video dims:
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (int(width), int(height)))
+    print(f"Source dimensions: {width}x{height}")
+    return cap, width, height
 
-    print(f"Video dimensions: {width}x{height}")
+def check_video(cap):
+    """
+    checks if the source opened properly, returns bool
+    """
+    if not cap.isOpened():
+        return False
+    print(f"Source opened successfully.")
+    return True
+
+def bound_set(width, bound):
+    """
+    sets the boundaries for camera movement based on the width of the video
+    """
     width_lower_bound = width - (bound * width)
     width_upper_bound = 0 + (bound * width)
     print(f"width lower = {width_lower_bound}, width upper = {width_upper_bound} ")
+    return width_lower_bound, width_upper_bound
+
+def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=False, frame_skip=5, gui=True, debug=False, bound=0.1):
+    """
+    works for both camera and video input!
+    video_path: 0 for camera, or path to video file
+    frame_skip_en: if True, processes every xth frame (helps performance)
+    frame_skip: the number of frames to skip
+    gui: shows video processing
+    debug: prints model info (too much info)
+    """
+    # load YOLO
+    model = YOLO(model)
+
+    # start video capture,
+    cap, width, height = get_video_dims(video_path)
+
+    # check if video is opened successfully,
+    if check_video(cap) == False:
+        print(f"Error: Couldn't open {type} file.")
+        return
+    
+    # output processing video,
+    out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (int(width), int(height)))
+
+    # set bounds for camera movement,
+    width_lower_bound, width_upper_bound = bound_set(width, bound)
 
     frame_count = 0
     while True:
         ret, frame = cap.read()  # get the frame
 
-        if not ret:  # if video ends or fails to grab frame
+        if not ret:  # if video ends or fails to grab frame,
             print("Failed to grab frame")
             break
 
-        # process every xth frame to reduce load
-        if debuff_en:
+        # process every xth frame to reduce load, enable with [frame_skip_en & frame_skip],
+        if frame_skip_en:
             frame_count += 1
-            if frame_count % debuff_range != 0:
+            if frame_count % frame_skip != 0:
                 continue
 
         # gather data,
         results = model(frame, verbose=debug)
-        boxes = results[0].boxes  # get the boxes
-        labels = boxes.cls  # get labels
-        confidences = boxes.conf  # get confidence scores
+        boxes = results[0].boxes    # get the boxes
+        labels = boxes.cls          # get labels
+        confidences = boxes.conf    # get confidence scores
 
         # filter out non-person detections + store coordinates,
         x_avg_list = [] # to store average x coordinate
         for i in range(len(boxes)):
             if labels[i] == 0:  # class 0 is person
-                x1, y1, x2, y2 = boxes.xyxy[i].cpu().numpy()  # get box coordinates
+                x1, y1, x2, y2 = boxes.xyxy[i].cpu().numpy()            # get box coordinates
                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 3)  # draw rect.
-                cv2.putText(frame, f"Person {i} Conf: {confidences[i]:.2f}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # add text
-                x_avg = round((x1 + x2) / 2, 2)  # average x-coordinate of each person
+                cv2.putText(frame, f"Person {i} Conf: {confidences[i]:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # add text
+                x_avg = round((x1 + x2) / 2, 2)                         # average x-coordinate of each person
                 x_avg_list.append(x_avg)
                 # print(f"Person {i} is at: {x_avg}")
 
+        # write to output,
         out.write(frame)
         print_info(x_avg_list, width_lower_bound, width_upper_bound)
             
@@ -71,71 +111,14 @@ def process_video(video_path, model="yolov5s.pt", debuff_en=False, debuff_range=
             cv2.imshow("Video Processing", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):  # exit gui
                 break
-
+    
+    # release everything,
     out.release()             
     cap.release()
     cv2.destroyAllWindows()
 
-def process_camera(camera=0, model="yolov5s.pt", debuff_en=False, debuff_range=1, gui=True, debug=False, bound=0.1):
-
-    model = YOLO(model)  # yolov5s is a smaller model than detr
-
-    # initialize camera:
-    cap = cv2.VideoCapture(camera)  # might have to change camera '#' later
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    width_lower_bound = width - (bound * width)
-    width_upper_bound = 0 + (bound * width)
-    print("Camera dimensions:", (width, height)) 
-    print(f"width lower = {width_lower_bound}, width upper = {width_upper_bound} ")
-
-    # variables:
-    frame_count = 0
-
-    # start reading frames,
-    while True:
-
-        ret, frame = cap.read() # get each frame,
-
-        if not ret: # if camera gets disconnected/failure,
-            print("Failed to grab frame")
-            break
-
-        # process every xth frame to reduce load, [debuff_en]
-        if debuff_en:
-            frame_count += 1
-            if frame_count % debuff_range != 0:
-                continue
-
-        # use YOLO model:
-        results = model(frame, verbose=debug) # turn on verbose with [debug]
-        boxes = results[0].boxes    # get results,
-        labels = boxes.cls          # labels found
-        confidences = boxes.conf    # confidence numbers
-
-        # filter out other objects detected, class 0 -> people in COCO
-        x_avg_list = [] # to store average x coordinate
-        for i in range(len(boxes)):
-            if labels[i] == 0:  # if a person,
-                x1, y1, x2, y2 = boxes.xyxy[i].cpu().numpy()  # unpack the coordinates
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 3) # draw the rect.
-                cv2.putText(frame, f"Person {i} Conf: {confidences[i]:.2f}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # add text
-                x_avg = round((x1 + x2) / 2, 2)  # average x-coordinate of each person
-                x_avg_list.append(x_avg)
-                x_avg = round((x1+x2)/2,2)  # middle x coordinate
-                # print(f"Person {i} is at: {x_avg}")
-        print_info(x_avg_list, width_lower_bound, width_upper_bound)
-
-        # debug gui: enable with [gui]
-        if gui:
-            cv2.imshow("Debug GUI", frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'): # exit gui
-                break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-# start operation:
-process_camera(bound=0.25)
-video_path = './Tracking_SW/archive/faceoff.mov'
-process_video(video_path, bound=0.25)
+if __name__ == '__main__':
+    # start operation:
+    # video_path = './Tracking_SW/archive/faceoff.mov'
+    video_path = 0
+    process_video(video_path, bound=0.25, frame_skip_en=True, frame_skip=5)
