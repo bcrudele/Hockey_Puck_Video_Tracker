b71432e4f6936c134a2b1d69961807508f68e1ba
bcrudele <b.crudele627@gmail.com>
2025-03-08 22:55:20 -0500

    Multi-Threaded + Queue

--- Tracking_SW/trackPeople.py
+++ Tracking_SW/trackPeople.py
@@ -1,6 +1,8 @@
 import cv2
 from ultralytics import YOLO  # pip install ultralytics
 from send_data import send_command # to send serial data
+from queue import Queue
+import threading
 
 def print_info(x_avg_list, width_lower_bound, width_upper_bound):
     """
@@ -25,8 +27,10 @@ def get_video_dims(video_path):
     cap = cv2.VideoCapture(video_path)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(f"FPS: {fps}")
     print(f"Source dimensions: {width}x{height}")
-    return cap, width, height
+    return cap, width, height, fps
 
 def check_video(cap):
     """
@@ -83,6 +87,30 @@ def determine_angle(angle, x_avg_list, width_lower_bound, width_upper_bound, cam
         
     return angle
 
+def frame_enqueue(video_path, frame_queue):
+    """
+    adds frames to the queue
+    """
+    cap = cv2.VideoCapture(video_path)
+    
+    # check if video is opened successfully,
+    if check_video(cap) == False:
+        print(f"Error: Couldn't open file.")
+        return
+    
+    while True:
+        ret, frame = cap.read() # get the frame
+
+        if not ret:
+            print("Failed to grab frame")
+            break
+
+        # add frame to queue,
+        frame_queue.put(frame)
+    
+    # when finished,
+    cap.release()
+
 def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=True, frame_skip=5, gui=True, debug=True, bound=0.1):
     """
     works for both camera and video input!
@@ -95,17 +123,17 @@ def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=True, frame_sk
     # load YOLO
     model = YOLO(model)
 
-    # start video capture,
-    cap, width, height = get_video_dims(video_path)
+    # set up FIFO queue for frames:
+    frame_queue = Queue()
+    frame_thread = threading.Thread(target=frame_enqueue, args=(video_path, frame_queue))
+    frame_thread.start()
 
-    # check if video is opened successfully,
-    if check_video(cap) == False:
-        print(f"Error: Couldn't open file.")
-        return
+    # start video capture,
+    cap, width, height, fps = get_video_dims(video_path)
     
     # output processing video,
-    box_render = cv2.VideoWriter('box_render.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 10, (int(width), int(height)))
-    original_film = cv2.VideoWriter('original_film.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 20, (int(width), int(height)))
+    #box_render = cv2.VideoWriter('box_render.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps//frame_skip, (int(width), int(height)))
+    original_film = cv2.VideoWriter('original_film.mp4', cv2.VideoWriter_fourcc(*'mp4v'),  fps, (int(width), int(height)))
 
     # set bounds for camera movement,
     width_lower_bound, width_upper_bound = bound_set(width, bound)
@@ -113,14 +141,17 @@ def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=True, frame_sk
     frame_count = 0
     current_angle = 90 # start at this angle
     while True:
-        ret, frame = cap.read()  # get the frame
 
-        if not ret:  # if video ends or fails to grab frame,
-            print("Failed to grab frame")
-            break
+        if frame_queue.empty():
+            #print("Queue is empty, waiting for frames...")
+            continue
+
+        # get frame from queue,
+        frame = frame_queue.get() 
+        #print(f"Queue Size: {frame_queue.qsize()}")
 
         original_film.write(frame)
-
+    
         # process every xth frame to reduce load, enable with [frame_skip_en & frame_skip],
         if frame_skip_en:
             frame_count += 1
@@ -138,14 +169,14 @@ def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=True, frame_sk
         for i in range(len(boxes)):
             if labels[i] == 0:  # class 0 is person
                 x1, y1, x2, y2 = boxes.xyxy[i].cpu().numpy()            # get box coordinates
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 3)  # draw rect.
-                cv2.putText(frame, f"Person {i} Conf: {confidences[i]:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # add text
+                #cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 3)  # draw rect.
+                #cv2.putText(frame, f"Person {i} Conf: {confidences[i]:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # add text
                 x_avg = round((x1 + x2) / 2, 2)                         # average x-coordinate of each person
                 x_avg_list.append(x_avg)
                 # print(f"Person {i} is at: {x_avg}")
 
         # write to output,
-        box_render.write(frame)
+        #box_render.write(frame)
 
         # calculate new angle
         angle = determine_angle(current_angle, x_avg_list, width_lower_bound, width_upper_bound, width)
@@ -168,7 +199,7 @@ def process_video(video_path=0, model="yolov5s.pt", frame_skip_en=True, frame_sk
                 break
     
     # release everything,
-    box_render.release() 
+    #box_render.release() 
     original_film.release()             
     cap.release()
     cv2.destroyAllWindows()
@@ -178,4 +209,4 @@ if __name__ == '__main__':
     # video_path = './Tracking_SW/archive/faceoff.mov'
     video_path = 0
     # video_path = './archive/faceoff.mov'
-    process_video(video_path, frame_skip_en=True, frame_skip=3, gui=True, debug=False, bound=0.4)
+    process_video(video_path, frame_skip_en=True, frame_skip=5, gui=True, debug=False, bound=0.4)
