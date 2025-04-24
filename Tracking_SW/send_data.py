11b68c8e10a7652a9cb823e7bd9ee3fe84d93e81
bcrudele <bcrudele@purdue.edu>
2025-04-24 18:02:27 -0400

    Clean COM funct.

--- Tracking_SW/send_data.py
+++ Tracking_SW/send_data.py
@@ -3,12 +3,7 @@ import serial
 def send_command(angle):
     COM_PORT = "COM3"
     ser = serial.Serial(COM_PORT, 115200, timeout=1)
-
-    # while True:
-    # key = input("Input ")
     ser.write(f"{angle}\n".encode())
-        #response = ser.readline().decode().strip()
-        #print("ESP32 Response:", response)
 
 
 def lcd_set(status):
@@ -18,6 +13,5 @@ def lcd_set(status):
         send_command(300)
     elif status == "recording_on":
         send_command(400)
-    elif status  == "recodirng_off":
+    elif status  == "recording_off":
         send_command(500)
-    
